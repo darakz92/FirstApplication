8771594ad3100de2f0c3a188980bcae8789e547e
darakz92 <darakz92@mail.ru>
2024-08-21 09:23:50 +0500

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Darkhan";
+            
+            byte age = 32;
+            
+            bool have_pet = true;
+            
+            double shoe_size = 40.0;
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("MyAge " + age);
+            Console.WriteLine("Do I have a pet? " + have_pet);
+            Console.WriteLine("My shoe size is " + shoe_size);
+        }
+    }
+}
