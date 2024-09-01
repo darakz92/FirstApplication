5217868ee4e96daa6359ca16b206808fe790c00e
darakz92 <darakz92@mail.ru>
2024-09-01 16:15:02 +0500

    Финальная практика модуля «Анкетируем пользователей»

--- FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/Program.cs
@@ -6,20 +6,19 @@ namespace FirstApplication.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string name = "Darkhan";
-            
-            byte age = 32;
-            
-            bool have_pet = true;
-            
-            double shoe_size = 40.0;
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
 
-            Console.WriteLine("My name is " + name);
-            Console.WriteLine("MyAge " + age);
-            Console.WriteLine("Do I have a pet? " + have_pet);
-            Console.WriteLine("My shoe size is " + shoe_size);
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
 
-            //end
+            Console.WriteLine("Enter date of Birth in format Day/Month/year: ");
+
+            DateOnly date;
+            date = DateOnly.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your name is {0} and age is {1} and date of birth {2} ", name, age, date);
+            Console.ReadKey();
         }
     }
 }
